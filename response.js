fe71b8c7099e69860e91509485a425bd2b9f3745
Yuda <Yuda@gmail.com>
2023-04-02 23:26:52 +0700

    Setup express js

---
+++ response.js
@@ -0,0 +1,17 @@
+const response = (statuscode,data,message,res) => {
+    res.json(statuscode,[
+        {
+            payload: {
+                data,
+                message,
+            },
+            metadata:{
+                prev: "",
+                next: "",
+                current: "",
+            },
+        },
+    ])
+}
+
+module.exports = response
